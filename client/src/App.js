ddf73cf3df1c36223282cbc09170f061b1c67e25
stanislaw.andryskowski <s.andryskowski@gmail.com>
2021-11-27 12:09:33 +0100

    added messages component

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import UsersPanel from './components/UsersPanel/UsersPanel';
 import PlacesPanel from './components/PlacesPanel/PlacesPanel';
 import UserProfile from './components/UserProfile/UserProfile';
 import { UsersContext } from './contexts/UsersContext';
+import Messages from './components/Messages/Messages';
 
 const CURRENT_USER = JSON.parse(window.localStorage.getItem('CURRENT_USER'));
 const CURRENT_USER_ID = CURRENT_USER ? CURRENT_USER._id : false;
@@ -36,8 +37,9 @@ function App() {
           ) : (
             <Route exact path="/" component={LandingPage} />
           )}
+          <PrivateRoute exact path="/messages" component={Messages} />
           <PrivateRoute exact path={`/edit/${CURRENT_USER_ID}`} component={UserPanel} />
-          <Route exact path="/userProfile" component={UserProfile} />
+          <PrivateRoute exact path="/userProfile" component={UserProfile} />
           {PLACES ? PLACES.map((place) => (
             <PrivateRoute
               exact
---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+import React, { useContext, useEffect } from 'react';
+import Parser from 'html-react-parser';
+import { UsersContext } from '../../contexts/UsersContext';
+
+function Messages(props) {
+  return (
+    <>
+      <h1>Messages</h1>
+    </>
+  );
+}
+
+export default Messages;
